cd2656afe92de1843af475879cf2c9a389178d12
ajay-pawan <ajaypawanrao@gmail.com>
2025-01-27 17:42:16 +0530

    Update ad-hoc-requests-3.sql

--- ad-hoc-requests-3.sql
+++ ad-hoc-requests-3.sql
@@ -1,18 +1,4 @@
-SELECT * FROM trips_db.dim_repeat_trip_distribution;
 
-select city_id,
-sum(case when trip_count="2-Trips" then repeat_passenger_count else null end) as 2_trips,
-sum(case when trip_count="3-Trips" then repeat_passenger_count else null end) as 3_trips,
-sum(case when trip_count="4-Trips" then repeat_passenger_count else null end) as 4_trips,
-sum(case when trip_count="5-Trips" then repeat_passenger_count else null end) as 5_trips,
-sum(case when trip_count="6-Trips" then repeat_passenger_count else null end) as 6_trips,
-sum(case when trip_count="7-Trips" then repeat_passenger_count else null end) as 7_trips,
-sum(case when trip_count="8-Trips" then repeat_passenger_count else null end) as 8_trips,
-sum(case when trip_count="9-Trips" then repeat_passenger_count else null end) as 9_trips,
-sum(case when trip_count="10-Trips" then repeat_passenger_count else null end) as 10_trips,
-sum(repeat_passenger_count) as total
-from dim_repeat_trip_distribution rtt
-group by city_id;
 
 
 
